a832d506ece5e12b40832f4fbd414c8f4b219976
Chris <hakizachrist1@gmail.com>
2023-02-07 18:11:27 +0200

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+import './style.css';
+
+document.querySelector('.displayscore').innerHTML = `<ul class="listscore">
+<li>name: 100</li>
+<li>name: 50</li>
+<li>name: 300</li>
+</ul>`
