e21169d190e5ae0051bc8bd077ff3abe0fa5a76f
Chris <hakizachrist1@gmail.com>
2023-02-08 16:09:33 +0200

    Update localstorage.js

--- src/Localstorage.js
+++ src/Localstorage.js
@@ -3,7 +3,7 @@ export default class LocalStorage {
     return JSON.parse(localStorage.getItem('ScoreData'));
   }
 
-  static saveData(ScoreData) {
-    localStorage.setItem('ScoreData', JSON.stringify(ScoreData));
+  static saveData(data) {
+    localStorage.setItem('ScoreData', JSON.stringify(data));
   }
 }
