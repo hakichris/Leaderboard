7e54d8462970acbc118a10b2ec913f2de610c3e8
Chris <hakizachrist1@gmail.com>
2023-02-08 10:09:15 +0200

    Update Localstorage

--- src/Localstorage.js
+++ src/Localstorage.js
@@ -1,9 +1,9 @@
 export default class LocalStorage {
-    static getData() {
-      return JSON.parse(localStorage.getItem('ScoreData'));
-    }
-  
-    static saveData(ScoreData) {
-      localStorage.setItem('ScoreData', JSON.stringify(ScoreData));
-    }
+  static getData() {
+    return JSON.parse(localStorage.getItem('ScoreData'));
   }
+
+  static saveData(ScoreData) {
+    localStorage.setItem('ScoreData', JSON.stringify(ScoreData));
+  }
+}
