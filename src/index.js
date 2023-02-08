7b888cc4e7e94de1c8fed780f0d4f686ab9225e7
Chris <hakizachrist1@gmail.com>
2023-02-08 15:17:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import './style.css';
+import LocalStorage from './Localstorage.js';
+import score from './Score.js'
+import UI from './UI';
 
-document.querySelector('.displayscore').innerHTML = `<ul class="listscore">
-<li>name: 100</li>
-<li>name: 50</li>
-<li>name: 300</li>
-</ul>`;
+let Data;
+if (LocalStorage.getData() === null) {
+  Data = [];
+} else {
+  Data = LocalStorage.getData();
+}
+
+const getInput = () => {
+    const nameinput = document.getElementById('name');
+    const scoreinput = document.getElementById('number');
+    const newInput = new score(nameinput.value, scoreinput.value);
+    nameinput.value = '';
+    scoreinput.value = '';
+    return newInput;
+  };
+
+const addscore = (input) => {
+    Data = LocalStorage.getData();
+    Data.push(input);
+    LocalStorage.saveData(Data);
+    // UI.showAllTasks(input);
+  };
+
+const addNewinput = document.querySelector('#submit');
+addNewinput.addEventListener('click', () => {
+   const newinput = getInput();
+    addscore(newinput);
+    UI.showAllTasks(newInput);
+  });
+
+
+  window.onload = () => {
+    Data.load();
+  };
