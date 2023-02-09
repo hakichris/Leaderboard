f690a6d993034b883752ac82a9331cc96458b701
Chris <hakizachrist1@gmail.com>
2023-02-09 11:55:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 import './style.css';
-import LocalStorage from './Localstorage.js';
-import score from './Score.js'
+import API from './APIrequest';
+import score from './Score.js';
 import UI from './UI';
 
 let Data = [];
-if (LocalStorage.getData() === null) {
+if (API.getData() === null) {
   Data = [];
 } else {
-  Data = LocalStorage.getData();
+  Data = API.getData();
 }
 
 const getInput = () => {
-    const nameinput = document.getElementById('name');
-    const scoreinput = document.getElementById('number');
-    const newInput = new score(nameinput.value, scoreinput.value);
-    nameinput.value = '';
-    scoreinput.value = '';
-    return newInput;
-  };
+  const nameinput = document.getElementById('name');
+  const scoreinput = document.getElementById('number');
+  const newInput = new score(nameinput.value, scoreinput.value);
+  nameinput.value = '';
+  scoreinput.value = '';
+  return newInput;
+};
 
 const addscore = (input) => {
-    Data.push(input);
-    LocalStorage.saveData(Data);
-  };
+  Data.push(input);
+  API.saveData(Data);
+};
 
 const addNewinput = document.getElementById('submit');
-addNewinput.addEventListener('click', () => {
-   const newinput = getInput();
-    addscore(newinput);
-    UI.showAllTasks(newinput);
-  });
- 
+addNewinput.addEventListener('submit', (e) => {
+    // e.preventDefault();
+  const newinput = getInput();
+  addscore(newinput);
+  UI.showAllTasks(newinput);
+});
 
 const btnRefresh = document.querySelector('#refresh');
 btnRefresh.addEventListener('click', () => {
@@ -38,14 +38,14 @@ btnRefresh.addEventListener('click', () => {
   UI.reloadPage();
 });
 
-const load =() => {
-  let datalist = LocalStorage.getData();
-    if (datalist === null) {
-      datalist = [];
-    }
-    datalist.forEach((input) => UI.showAllTasks(input));
-}
+const load = () => {
+  let datalist = API.getData();
+  if (datalist === null) {
+    datalist = [];
+  }
+  datalist.forEach((input) => UI.showAllTasks(input));
+};
 
-  window.onload = () => {
-    load();
-  };
+window.onload = () => {
+  load();
+};
