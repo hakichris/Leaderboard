5c33caaa984f8f0c4d6b5ffaa58d58d2e8eae7ae
Chris <hakizachrist1@gmail.com>
2023-02-09 15:13:12 +0200

    Fix linters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './style.css';
 import API from './modules/APIrequest';
 import UI from './modules/UI';
 
-
 const adding = document.getElementById('submit');
 const refresh = document.getElementById('refresh');
 const score = document.getElementById('score');
@@ -11,19 +10,16 @@ const user = document.getElementById('user');
 adding.addEventListener('click', async (e) => {
   e.preventDefault();
   await API.saveData(user.value, Number(score.value));
-  console.log(user.value, score.value)
+  console.log(user.value, score.value);
   user.value = '';
   score.value = '';
 });
 
 refresh.addEventListener('click', async () => {
   window.location.reload();
-  UI.displayScore();;
+  UI.displayScore();
 });
 
 document.addEventListener('DOMContentLoaded', async () => {
-  UI.displayScore();;
+  UI.displayScore();
 });
-
-
-
--- src/modules/APIrequest.js
+++ src/modules/APIrequest.js
@@ -1,35 +1,33 @@
-export default class API {    
+export default class API {
   static saveData = async (user, userScore) => {
-        const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-        const id = '0oB7cziYAqgRf6ROSq4e';
-        const identifierUrl = `${url}games/${id}/scores/`;
+    const url =
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+    const id = '0oB7cziYAqgRf6ROSq4e';
+    const identifierUrl = `${url}games/${id}/scores/`;
 
-          const response = await fetch(identifierUrl, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ 
-              user: `${user}`, 
-              score: userScore 
-            }),
-          });
-          const res = await response.json();
-          console.log("check the data here:", res)
-          return res.result;
-        };
-        
-  
+    const response = await fetch(identifierUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: `${user}`,
+        score: userScore,
+      }),
+    });
+    const res = await response.json();
+    console.log('check the data here:', res);
+    return res.result;
+  };
 
- static getScore = async () => {
-      const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-      const id = '0oB7cziYAqgRf6ROSq4e';
-      const identifierUrl = `${url}games/${id}/scores/`;
+  static getScore = async () => {
+    const url =
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+    const id = '0oB7cziYAqgRf6ROSq4e';
+    const identifierUrl = `${url}games/${id}/scores/`;
 
-      const response = await fetch(identifierUrl, {
-      });
-      const res = await response.json();
-      return res.result;
-    };
+    const response = await fetch(identifierUrl, {});
+    const res = await response.json();
+    return res.result;
+  };
 }
-
